37e84e11d1288e0091aab4defd7756974b65913e
Muhammad Adif Fikri <fikriadif7@gmail.com>
2022-09-18 18:49:24 +0700

    fix litepicker, fix navabar store equipment

--- app/Views/template_worker/navbar.php
+++ app/Views/template_worker/navbar.php
@@ -498,7 +498,7 @@
                                             </div>
                                         </div>
 
-                                        <a class="dropdown-item" href="./accordion.html">
+                                        <a class="dropdown-item" href="<?= base_url('storeEquipment')?>">
                                             <span class="nav-link-icon d-md-none d-lg-inline-block">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-basket" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
